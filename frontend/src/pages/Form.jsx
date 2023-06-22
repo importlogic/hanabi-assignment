6750c188e1094ee44907143bc58c747d0e17c4f2
Manas Rawat <manasrawat.cse@gmail.com>
2023-06-22 21:08:18 +0530

    null number issue fixed

--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -34,7 +34,7 @@ const Form = () => {
             } else if (response.data != null) {
                 setName(response.data.name);
                 setEmail(response.data.email);
-                setNumber(String(response.data.number));
+                response.data.number == null ? setNumber('') : setNumber(String(response.data.number));  
                 setDob(response.data.dob);
             }
 
